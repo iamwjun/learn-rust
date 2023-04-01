95154e0480a966559c0d95b0dd820da79319d28f
iamwjun <cddxwujun@hotmail.com>
2023-04-01 12:32:55 +0800

    chore: learn struct

--- 06-structs/defining/src/main.rs
+++ 06-structs/defining/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
         active: false,
         username: String::from("wjun"),
         email: String::from("test@hotmail.com"),
-        sign_in_count: 1
+        sign_in_count: 1,
     };
 
     println!("user1 username is {}", user1.username);
@@ -18,4 +18,20 @@ fn main() {
     user1.email = String::from("demo@hotmail.com");
 
     println!("user1 email is {}", user1.email);
+
+    let user2 = User {
+        email: String::from("dodo@hotmail.com"),
+        ..user1
+    };
+
+    let user3 = build_user(String::from("dodo"), String::from("koukou@hotmail.com"));
+}
+
+fn build_user(username: String, email: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
 }
