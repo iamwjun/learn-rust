e7145e8fc542348b295f5422744da7d8e196a2e8
iamwjun <cddxwujun@hotmail.com>
2023-04-06 09:45:12 +0800

    chore: learn define structs 02

--- 06-structs/defining/src/main.rs
+++ 06-structs/defining/src/main.rs
@@ -5,6 +5,12 @@ struct User {
     sign_in_count: u64,
 }
 
+struct Color(i32, i32, i32);
+
+struct Point(i32, i32, i32);
+
+struct AlwaysEqual;
+
 fn main() {
     let mut user1 = User {
         active: false,
@@ -17,14 +23,29 @@ fn main() {
 
     user1.email = String::from("demo@hotmail.com");
 
-    println!("user1 email is {}", user1.email);
+    println!(
+        "user1 info: username -> {}, email -> {}, active -> {}, sign_in_count -> {}",
+        user1.username, user1.email, user1.active, user1.sign_in_count
+    );
 
     let user2 = User {
         email: String::from("dodo@hotmail.com"),
         ..user1
     };
 
+    println!("user2 email is {}", user2.email);
+
     let user3 = build_user(String::from("dodo"), String::from("koukou@hotmail.com"));
+
+    println!("user3 email is {}", user3.email);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    println!("black value is {}", black.0);
+    println!("origin value is {}", origin.0);
+
+    let alway = AlwaysEqual;
 }
 
 fn build_user(username: String, email: String) -> User {
