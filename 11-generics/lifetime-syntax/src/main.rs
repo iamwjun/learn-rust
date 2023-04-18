2141045a5354de6a380d29b103c7d42e5509b8db
iamwjun <cddxwujun@hotmail.com>
2023-04-18 17:48:54 +0800

    chore: learn lifetime syntax

--- 11-generics/lifetime-syntax/src/main.rs
+++ 11-generics/lifetime-syntax/src/main.rs
@@ -11,9 +11,22 @@ fn main() {
 
     let result = longest(string1.as_str(), string2);
     println!("The longest string is {}", result);
+
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
 }
 
-fn longest(x: &str, y: &str) -> &str {
+fn longest<'a>(x: &str, y: &str) -> &'a str {
+    let result = String::from("really long string");
+    result.as_str()
+}
+
+fn longest2<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
