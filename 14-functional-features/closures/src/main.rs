9edbf79fe856e922d36ebb6feec72329d897e911
iamwjun <cddxwujun@hotmail.com>
2023-04-24 18:02:31 +0800

    chore: learn closures

--- 14-functional-features/closures/src/main.rs
+++ 14-functional-features/closures/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq, Clone, Copy)]
 enum ShirtColor {
     Red,
     Blue
@@ -8,15 +9,60 @@ struct Inventory {
 }
 
 impl Inventory {
-    fn giveway() -> ShirtColor {
-        
+    fn giveway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
     }
 
-    fn most_stocked() -> ShirtColor {
-        ShirtColor::Red
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => num_red += 1,
+                ShirtColor::Blue => num_blue += 1,
+            }
+        }
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
+}
+
+struct  Solution {}
+
+impl Solution {
+    pub fn fib(n: i32) -> i32 {
+        match n {
+            1 | 2 => n,
+            _ => Solution::fib(n - 1) + Solution::fib(n - 2)
+        }
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let store = Inventory {
+        shirts: vec![ShirtColor::Red, ShirtColor::Blue, ShirtColor::Red]
+    };
+    
+    let user1 = Some(ShirtColor::Red);
+    let give1 = store.giveway(user1);
+
+    println!("user1 like {:?}, give him a {:?}!", user1, give1);
+
+    let user2 = None;
+    let give2 = store.giveway(user2);
+
+    println!("user2 like {:?}, give him a {:?}!", user2, give2);
+
+    let user3 = Some(ShirtColor::Blue);
+    let give3 = store.giveway(user3);
+
+    println!("user2 like {:?}, give him a {:?}!", user3, give3);
+
+    let solution = Solution {};
+    
+    println!("febo is {}", Solution::fib(5))
 }
