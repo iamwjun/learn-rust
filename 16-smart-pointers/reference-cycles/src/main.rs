7a673376f74f8a8d63cf39bb63180988d801baba
iamwjun <cddxwujun@hotmail.com>
2023-05-26 17:48:20 +0800

    chore: learn reference cycles

---
+++ 16-smart-pointers/reference-cycles/src/main.rs
@@ -0,0 +1,25 @@
+use crate::List::{Cons, Nil};
+use std::{cell::RefCell, rc::Rc};
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
+}
+
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, item) => Some(&item),
+            Nil => None
+        }
+    }
+}
+
+fn main() {
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+    println!("a is {:#?}", a);
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+}
