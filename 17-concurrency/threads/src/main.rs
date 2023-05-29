041ebc39eebfad5465878edb8bd975de72823cb0
iamwjun <cddxwujun@hotmail.com>
2023-05-29 17:40:13 +0800

    chore: learn thread basic

---
+++ 17-concurrency/threads/src/main.rs
@@ -0,0 +1,26 @@
+use std::{thread, time::Duration};
+
+fn main() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    handle.join().unwrap();
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    };
+
+    let a = vec![1, 2, 3];
+    let b = thread::spawn(move || {
+        println!("a is {:?}", a);
+    });
+
+    drop(a); // error
+
+    b.join().unwrap();
+}
