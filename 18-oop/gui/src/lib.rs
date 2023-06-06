2682a7eb844019285babe77cde6e34c73da23b0b
iamwjun <cddxwujun@hotmail.com>
2023-06-06 16:32:53 +0800

    chore: learn creat gui

--- 18-oop/gui/src/lib.rs
+++ 18-oop/gui/src/lib.rs
@@ -2,11 +2,14 @@ pub trait Draw {
     fn draw(&self);
 }
 
-pub struct Screen {
-    pub components: Vec<Box<dyn Draw>>
+pub struct Screen<T: Draw> {
+    pub components: Vec<T>,
 }
 
-impl Screen {
+impl<T> Screen<T>
+where
+    T: Draw,
+ {
     pub fn run(&self) {
         for component in self.components.iter() {
             component.draw();
