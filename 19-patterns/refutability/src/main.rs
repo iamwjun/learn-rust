c347278d58d66243258c92afce2fb503e3204a0f
iamwjun <cddxwujun@hotmail.com>
2023-06-14 16:16:10 +0800

    chore: learn refutability

---
+++ 19-patterns/refutability/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // if let Some(x) = some_option_value {
+    //     println!("{}", x);
+    // }
+
+    println!("Hello, world!");
+
+    if let x = 5 {
+        println!("{}", x);
+    };
+}
