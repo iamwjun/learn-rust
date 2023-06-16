7c1a9c3b075a51b5848608fef2aff904a2375742
iamwjun <cddxwujun@hotmail.com>
2023-06-16 18:17:30 +0800

    chore: learn unsafe rust 02

--- 20-advanced-features/unsafe-rust/src/main.rs
+++ 20-advanced-features/unsafe-rust/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let mut num = 5;
+
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+    }
+
+    println!("r1 is {:#?}, r2 is {:#?}", r1, r2);
+
+    let address = 0x12345usize;
+    // let r = &address as *const i32;
 }
