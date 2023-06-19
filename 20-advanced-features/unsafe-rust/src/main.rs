3ea54dc3fc6f4bdfeb24d1ffada20769973419e2
iamwjun <cddxwujun@hotmail.com>
2023-06-19 15:32:07 +0800

    chore: learn unsafe rust 03

--- 20-advanced-features/unsafe-rust/src/main.rs
+++ 20-advanced-features/unsafe-rust/src/main.rs
@@ -1,3 +1,5 @@
+use std::slice;
+
 fn main() {
     let mut num = 5;
 
@@ -11,6 +13,32 @@ fn main() {
 
     println!("r1 is {:#?}, r2 is {:#?}", r1, r2);
 
-    let address = 0x12345usize;
+    let _address = 0x12345usize;
     // let r = &address as *const i32;
+
+    unsafe {
+        dangerous();
+    }
+
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+
+    let r = &mut v[..];
+
+    let (a, b) = split_at_mut(r, 3);
+
+    assert_eq!(a, &mut vec![1, 2, 3]);
+    assert_eq!(b, &mut vec![4, 5, 6]);
 }
+
+unsafe fn dangerous() {
+    println!("dangerous")
+}
+
+fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = values.len();
+
+    assert!(mid <= len);
+
+    (&mut values[..mid], &mut values[mid..])
+}
+
