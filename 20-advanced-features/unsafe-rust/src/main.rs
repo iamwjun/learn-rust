9635c56b4f91aa963515a3409a415d71e77413ac
iamwjun <cddxwujun@hotmail.com>
2023-06-19 15:52:07 +0800

    chore: learn unsafe rust extern

--- 20-advanced-features/unsafe-rust/src/main.rs
+++ 20-advanced-features/unsafe-rust/src/main.rs
@@ -20,14 +20,16 @@ fn main() {
         dangerous();
     }
 
-    let mut v = vec![1, 2, 3, 4, 5, 6];
+    let mut v = vec![1, 2, 3, 4, 5, 6, 7, 8];
 
     let r = &mut v[..];
 
     let (a, b) = split_at_mut(r, 3);
 
     assert_eq!(a, &mut vec![1, 2, 3]);
-    assert_eq!(b, &mut vec![4, 5, 6]);
+    assert_eq!(b, &mut vec![4, 5, 6, 7, 8]);
+
+    println!("a is {:#?}, b is {:#?}", a, b);
 }
 
 unsafe fn dangerous() {
@@ -36,9 +38,15 @@ unsafe fn dangerous() {
 
 fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
     let len = values.len();
+    let ptr = values.as_mut_ptr();
 
     assert!(mid <= len);
 
-    (&mut values[..mid], &mut values[mid..])
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid)
+        )
+    }
 }
 
