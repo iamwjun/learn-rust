410922c99c50270ec5e4a9877a11bd352e72b21e
iamwjun <cddxwujun@hotmail.com>
2023-06-20 15:39:21 +0800

    chore: learn rust macros 02

---
+++ 20-advanced-features/hello_macro/src/lib.rs
@@ -0,0 +1,18 @@
+pub trait HelloMacro {
+    fn hello_world();
+}
+
+pub fn add(left: i32, right: i32) -> i32 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
