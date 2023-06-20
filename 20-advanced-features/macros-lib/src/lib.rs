badac919a1fa6fa71afa8a79227ac38cb59d2567
iamwjun <cddxwujun@hotmail.com>
2023-06-20 15:29:31 +0800

    chore: learn rust macros

---
+++ 20-advanced-features/macros-lib/src/lib.rs
@@ -0,0 +1,34 @@
+use proc_macro;
+
+#[some_attribute]
+pub fn some_name(input: TokenStream) -> TokenStream {
+    
+}
+
+#[macro_export]
+macro_rules! vec {
+    ( $( $x:expr ),* ) => {
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push($x);
+            )*
+            temp_vec
+        }
+    };
+}
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
--- README.md
+++ README.md
@@ -6,3 +6,5 @@ awesome-rust https://github.com/Alez87/awesome-rust
 dua-cli https://github.com/Byron/dua-cli
 
 rust-algorithms https://github.com/EbTech/rust-algorithms
+
+rust-macros https://veykril.github.io/tlborm/syntax-extensions.html
