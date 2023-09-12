2983989930ab8a87f57f7bb20320332c0e000ee9
iamwjun <cddxwujun@hotmail.com>
2023-09-12 11:05:00 +0800

    docs: learn std io read line

---
+++ 22-again/02-guessing-game-tutorial/guessing_game/src/main.rs
@@ -0,0 +1,15 @@
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    println!("Please input a number!");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("read input number fail");
+
+    println!("you guessed number is: {guess}");
+}
