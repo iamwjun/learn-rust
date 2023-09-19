e8b72c5f9613feac8a407ddb4c5007df5b804c36
iamwjun <cddxwujun@hotmail.com>
2023-09-19 21:41:20 +0800

    docs: learn use nickel server

--- 23-practice-items/nickel-app/src/main.rs
+++ 23-practice-items/nickel-app/src/main.rs
@@ -1,3 +1,11 @@
+#[macro_use] extern crate nickel;
+
+use nickel::{Nickel, HttpRouter};
+
 fn main() {
-    println!("Hello, world!");
+    let mut server = Nickel::new();
+
+    server.get("/bar", middleware!("This is the /bar handler"));
+
+    server.listen("127.0.0.1:6767").unwrap();
 }
