fc981e1fdbb07179f656e9186889ece914dc7818
iamwjun <cddxwujun@hotmail.com>
2023-09-20 16:33:32 +0800

    docs: learn use nickel http router

--- 23-practice-items/nickel-app/src/main.rs
+++ 23-practice-items/nickel-app/src/main.rs
@@ -7,5 +7,13 @@ fn main() {
 
     server.get("/bar", middleware!("This is the /bar handler"));
 
+    server.get("/user/:userid", middleware! {|request|
+        format!("this user's id is {:?}", request.param("userid"))
+    });
+
+    server.get("/a/*/d", middleware! {"matchs /a/b/d not /a/b/c/d"});
+
+    server.get("/a/**/d", middleware! {"This matches /a/b/d and also /a/b/c/d"});
+
     server.listen("127.0.0.1:6767").unwrap();
 }
