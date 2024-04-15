712821c3d98fad3ebcab266890864af7b61f4727
iamwjun <cddxwujun@hotmail.com>
2024-04-15 16:36:04 +0800

    docs: hello world

---
+++ rust-course/hello-world/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    great_world();
+}
+
+fn great_world() {
+    let southern_germany = "Grüß Gott!";
+    let chinese = "世界，你好";
+    let english = "World, hello";
+    let regions = [southern_germany, chinese, english];
+    for region in regions.iter() {
+        println!("{}", &region);
+    }
+}
