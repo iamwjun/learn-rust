2f71357b326e47fe3cf50d267d6d0fb6c6118987
iamwjun <cddxwujun@hotmail.com>
2024-04-22 15:29:48 +0800

    docs: learn rust variable

--- rust-course/variable/src/main.rs
+++ rust-course/variable/src/main.rs
@@ -1,3 +1,5 @@
+use num::complex::Complex;
+
 struct Struct {
     e: i32
 }
@@ -41,7 +43,67 @@ fn main() {
 
     println!("u8 MAX is {}", u8::MAX.saturating_add(127));
 
-    let a : u8 = 255;
+    let a: u8 = 255;
     let b = a.wrapping_add(20);
-    println!("{}", b);  // 19
+    println!("{}, {}", b, u8::MAX);  // 19
+
+    let x = 3.1;
+    let y: f32 = 9.22;
+    println!("x is {}, y is {}", x, y);
+	
+	println!("0.1 + 0.2 = {}", 0.1 + 0.2);
+
+	let abc: (f32, f32, f32) = (0.1, 0.2, 0.3);
+    let xyz: (f64, f64, f64) = (0.1, 0.2, 0.3);
+
+    println!("abc (f32)");
+    println!("   0.1 + 0.2: {:x}", (abc.0 + abc.1).to_bits());
+    println!("         0.3: {:x}", (abc.2).to_bits());
+    println!();
+
+    println!("xyz (f64)");
+    println!("   0.1 + 0.2: {:x}", (xyz.0 + xyz.1).to_bits());
+    println!("         0.3: {:x}", (xyz.2).to_bits());
+    println!();
+
+	let x = (-42.0_f32).sqrt();
+	println!("-42.0 sqrt is {}", x);
+	if x.is_nan() {
+		println!("未定义的数学行为");
+	}
+
+	let one_million = 1_000_000;
+	println!("one million is {}", one_million);
+
+	let forty_twos = [
+		42.0,
+		42f32,
+		42.0_f32,
+	];
+	println!("{:.8}", forty_twos[0]);
+
+	let a:i32 = 2;
+    let b:i32 = 3;
+	println!("a is {:b}", a);
+	println!("b is {:b}", b);
+	println!("a & b is {:b}", a & b);
+	println!("a | b is {:b}", a | b);
+	println!("a ^ b is {:b}", a ^ b);
+	println!("!b is {:b}", !b);
+	println!("a >> b is {:b}", a >> b);
+	println!("a << b is {:b}", a << b);
+
+	for i in 1..=5 {
+		println!("i is {}", i);
+	}
+
+	for i in 'a'..='z' {
+		println!("{}", i);
+	}
+
+	let a = Complex { re: 2.1, im: -1.2 };
+	let b = Complex::new(11.1, 22.2);
+	let result = a + b;
+
+	println!("{} + {}i", result.re, result.im)
 }
