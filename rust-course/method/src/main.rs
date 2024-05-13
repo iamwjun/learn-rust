8fa835ef754c28485f209b1e2ef5df0fdaa97b52
iamwjun <cddxwujun@hotmail.com>
2024-05-13 15:19:15 +0800

    docs: learn use impl

--- rust-course/method/src/main.rs
+++ rust-course/method/src/main.rs
@@ -5,8 +5,6 @@ struct Circle {
 }
 
 impl Circle {
-    // new是Circle的关联函数，因为它的第一个参数不是self，且new并不是关键字
-    // 这种方法往往用于初始化当前结构体的实例
     fn new(x: f64, y: f64, radius: f64) -> Circle {
         Circle {
             x: x,
@@ -15,13 +13,55 @@ impl Circle {
         }
     }
 
-    // Circle的方法，&self表示借用当前的Circle结构体
     fn area(&self) -> f64 {
         std::f64::consts::PI * (self.radius * self.radius)
     }
 }
 
+pub struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    pub fn new(width: u32, height: u32) -> Rectangle {
+        Rectangle { width, height }
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn can_hold(self, other: Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+}
+
 
 fn main() {
-    println!("Hello, world!");
+    let rect1 = Rectangle { width: 30, height: 50 };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
+
+    let rect2 = Rectangle::new(30, 100);
+    println!("{}", rect2.height());
+
+    let rect3 = Rectangle::new(50, 60);
+    println!("2 can hold 3 {}", rect2.can_hold(rect3));
+
 }
