62fb11b6a3d4be4fe4d7bfc5f58575236838e0e9
iamwjun <cddxwujun@hotmail.com>
2024-05-17 13:51:08 +0800

    docs: learn rust advance trait

---
+++ rust-course/advance-trait/src/main.rs
@@ -0,0 +1,50 @@
+use std::{fmt, ops::Add};
+
+#[derive(Debug, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+struct Millimeters(u32);
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + (other.0 * 1000))
+    }
+}
+
+
+fn main() {
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 }
+    );
+
+    let point = Point {
+        x: 12,
+        y: 13
+    };
+
+    println!("{}", point);
+}
