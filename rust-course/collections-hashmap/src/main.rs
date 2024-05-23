1b8109a64257806cb9f41bd93129fac4f7dca178
iamwjun <cddxwujun@hotmail.com>
2024-05-23 22:02:57 +0800

    docs: learn rust collections hashmap

--- rust-course/collections-hashmap/src/main.rs
+++ rust-course/collections-hashmap/src/main.rs
@@ -8,4 +8,17 @@ fn main() {
     my_gems.insert("河边捡的误以为是宝石的破石头", 18);
 
     println!("my_gems is {:#?}", my_gems);
+
+    let teams_list = vec![
+        ("中国队".to_string(), 100),
+        ("美国队".to_string(), 10),
+        ("日本队".to_string(), 50),
+    ];
+
+    let mut teams_map = HashMap::new();
+    for team in &teams_list {
+        teams_map.insert(&team.0, team.1);
+    }
+
+    println!("{:?}",teams_map)
 }
