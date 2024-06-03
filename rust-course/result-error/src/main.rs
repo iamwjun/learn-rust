75c1f50da1d6a0e4b164f8e841e3bcadf53ba22e
iamwjun <cddxwujun@hotmail.com>
2024-06-03 18:11:51 +0800

    docs: learn rust result error

--- rust-course/result-error/src/main.rs
+++ rust-course/result-error/src/main.rs
@@ -1,10 +1,35 @@
-use std::net::IpAddr;
+use std::{fs::File, io::{self, ErrorKind, Read}};
 
 fn main() {
-    let v = vec![1, 2, 3];
+    let file = File::open("hello.txt");
 
-    // std::panic::panic_any("burn");
+    let file = match file {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => panic!("Problem opening the file: {:?}", other_error),
+        },
+    };
 
-    let home: IpAddr = "127.0.01".parse().unwrap();
-    println!("home ip is {}", home);
+    println!("file is {:#?}", file);
+
+    let content = read_username_from_file();
+    println!("content is {:#?}", content);
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
 }
