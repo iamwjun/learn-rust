3b6bf477737bd5b93355d68a00b3c6fab1c6fb8b
iamwjun <cddxwujun@hotmail.com>
2024-06-03 14:27:26 +0800

    docs: learn rust life time

--- rust-course/lifetime/src/main.rs
+++ rust-course/lifetime/src/main.rs
@@ -1,15 +1,49 @@
+#[derive(Debug)]
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn announce_and_return_part<'b>(&'a self, announcement: &'b str) -> &'b str
+    where
+        'a: 'b,
+    {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
 fn main() {
-    let r;
-    let x = 5;
-    r = &x;
+    let long_life: &'static str = "I have a long life time";
 
-    println!("r: {}", r);
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+    println!("part is {:?}", i.part);
+    println!("announce is {:?}", i.announce_and_return_part(&novel));
 
-    let string1 = String::from("abcd");
-    let string2 = "xyz";
+    let string1 = String::from("long string is long");
+    
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
 
-    let result = longest(string1.as_str(), string2);
-    println!("The longest string is {}", result);
+    let result;
+
+    {
+        let string2 = String::from("long string is abcdef");
+        result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
+
+    let s = String::from("abcd 1234");
+    println!("result is {}", first_word(&s));
+
+    println!("long_life's mean is {}", long_life);
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
@@ -18,5 +52,19 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     } else {
         y
     }
+}
+
+fn useless<'a>(a: &'a str, b: &'a str) {}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
 
