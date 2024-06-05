f70e1ab9d30a05e6428bcc6843bad5057e62fb35
iamwjun <cddxwujun@hotmail.com>
2024-06-05 13:58:07 +0800

    docs: learn rust chain call

--- rust-course/result-error/src/main.rs
+++ rust-course/result-error/src/main.rs
@@ -19,24 +19,34 @@ fn main() {
     let content = read_username_from_file();
     println!("content is {:#?}", content);
 
-    let mut f = match f {
-        // 打开文件成功，将file句柄赋值给f
-        Ok(file) => file,
-        // 打开文件失败，将错误返回(向上传播)
-        Err(e) => return Err(e),
-    };
+    println!("open file is {:#?}", open_file());
+
+    println!("read file is {:#?}", read_file());
+
+    let text = String::from("iamw");
+    println!("fist is {:#?}", last_char_of_first_line(&text));
 }
 
 fn read_username_from_file() -> Result<String, io::Error> {
-    let f = File::open("hello.txt");
-
-    let mut f = match f {
-        Ok(file) => file,
-        Err(e) => return Err(e),
-    };
+    let mut f = File::open("hello.txt")?;
     let mut s = String::new();
-    match f.read_to_string(&mut s) {
-        Ok(_) => Ok(s),
-        Err(e) => Err(e),
-    }
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn open_file() -> Result<File, Box<dyn std::error::Error>> {
+    let f = File::open("hello.txt")?;
+    Ok(f)
+}
+
+fn read_file() -> Result<String, io::Error> {
+    let mut s = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    Ok(s)
+}
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
 }
