5df4af5bf25ad6fdc6efa62083ed14bc2eb9d486
iamwjun <cddxwujun@hotmail.com>
2024-06-06 16:37:52 +0800

    docs: learn rust package and crate

---
+++ rust-course/crate-module/restaurant/src/lib.rs
@@ -0,0 +1,53 @@
+// mod front_of_house {
+//     mod hosting {
+//         fn add_to_waitlist() {}
+
+//         fn seat_at_table() {}
+//     }
+
+//     mod serving {
+//         fn take_order() {}
+
+//         fn serve_order() {}
+
+//         fn take_payment() {}
+//     }
+// }
+
+mod front_of_house {
+    pub(crate) mod hosting {
+        pub(crate) fn add_to_waitlist() {}
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // 绝对路径
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // 相对路径
+    front_of_house::hosting::add_to_waitlist();
+}
+
+use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant_02() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+}
+
+fn serve_order() {
+    self::back_of_house::cook_order()
+}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        crate::serve_order();
+    }
+
+    pub fn cook_order() {}
+}
+
+
+
